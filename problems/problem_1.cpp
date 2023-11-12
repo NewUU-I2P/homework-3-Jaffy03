0011962fe09cab58f5f1d60307dc0e1a05637db0
Jasurbek <j.kuandikov@newuu.uz>
2023-11-12 21:26:26 +0500

    hw3

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -2,11 +2,11 @@ float problemSolution1(float consumed_water) {
     float cost;
     // write your code here
     if(consumed_water>=60){
-        cost=13+30*0.4+20*0.12+10*1.4+(consumed_water-60)*1.5;
+        cost=13+30*0.4+20*1.2+10*1.4+(consumed_water-60)*1.5;
     }else if(consumed_water>=50&&consumed_water<60){
-        cost=13+30*0.4+20*0.12+(consumed_water-50)*1.4;
+        cost=13+30*0.4+20*1.2+(consumed_water-50)*1.4;
     }else if(consumed_water>=30&&consumed_water<50){
-        cost=13+30*0.4+(consumed_water-30)*0.12;
+        cost=13+30*0.4+(consumed_water-30)*1.2;
     }else if (consumed_water>0&&consumed_water<30) {
         cost =13+consumed_water * 0.4;
     }else{
